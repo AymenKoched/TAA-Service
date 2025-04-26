b31746f55b4c6cd550848b66223308a864de2cdb
Aymen Koched <aymenkoched@gmail.com>
2025-04-26 15:16:08 +0000

    fix: config

--- service/src/configuration.ts
+++ service/src/configuration.ts
@@ -1,7 +1,11 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { JwtModuleOptions } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 import { readFileSync } from 'fs';
-import { isEmpty } from 'lodash';
+import { isArray, isEmpty, isObject } from 'lodash';
 import { hostname } from 'os';
 import { resolve } from 'path';
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
@@ -65,35 +69,67 @@ conf.jwt.privateKey = readFileSync(resolve(conf.jwt.privateKeyPath)).toString(
 function loadFromDirectory<T extends AppConfig = AppConfig>(
   rootProjectPath: string,
 ): T {
-  let devConf;
   let prodConf;
 
   const confDirectory = resolve(rootProjectPath, './conf');
   const packagePath = resolve(rootProjectPath, './package.json');
-  const envDevConfigPath = resolve(confDirectory, './env-dev.json');
   const envConfigPath = resolve(confDirectory, './env.json');
 
   const projectPackage = JSON.parse(readFileSync(packagePath).toString());
 
   try {
-    devConf = JSON.parse(readFileSync(envDevConfigPath).toString());
+    console.log('env: ', process.env);
+    const prodConfStr = readFileSync(envConfigPath).toString();
+    prodConf = setEnvVars(JSON.parse(prodConfStr));
   } catch (e) {
-    devConf = {};
+    console.error(e);
+    prodConf = {};
   }
 
-  if (isEmpty(devConf)) {
-    try {
-      prodConf = JSON.parse(readFileSync(envConfigPath).toString());
-    } catch (e) {
-      console.error(e);
-      prodConf = {};
-    }
-  }
-
-  return Object.assign(devConf, prodConf, {
+  return Object.assign(prodConf, {
     app: {
       hostname: hostname(),
       appname: projectPackage.name,
     },
   });
+}
+
+function setEnvVars(original: any): any {
+  if (typeof original === 'string') {
+    const regex = new RegExp(/^\{\{(.+?)\}\}$/g);
+    const match = regex.exec(original);
+
+    if (isEmpty(match) || !match[1]) {
+      return original;
+    }
+    if (match[1].includes(':')) {
+      const parts = match[1].split(':');
+      switch (parts[1]?.toLowerCase()) {
+        case 'array':
+          return getEnvArray(parts[0]);
+        case 'number':
+          return Number(process.env[parts[0]]);
+      }
+    }
+    return process.env[match[1]] || match[1];
+  }
+
+  if (isArray(original)) {
+    return original.map((item) => setEnvVars(item));
+  }
+
+  if (isObject(original)) {
+    return Object.keys(original).reduce(
+      (acc, key) => ({ ...acc, [key]: setEnvVars(original[key]) }),
+      {},
+    );
+  }
+
+  return original;
+}
+
+function getEnvArray(varName: string): string[] {
+  return Object.keys(process.env)
+    .filter((key) => key.startsWith(varName))
+    .map((key) => process.env[key]);
 }
